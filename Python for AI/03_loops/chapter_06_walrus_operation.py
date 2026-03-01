722f39ba8ac33e944f2a64ce6323ffd12a50c4ac
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-01 14:54:02 +0600

    Walrus operation-2

--- Python for AI/03_loops/chapter_06_walrus_operation.py
+++ Python for AI/03_loops/chapter_06_walrus_operation.py
@@ -9,3 +9,7 @@ print(f"Available flavourse are {available_flavours}.")
 while(flavour := input("Please select one flavour: ").lower()) not in available_flavours:
     print(f"The flavour- {flavour} isn't available. Please select another one")
 print(f"You have selected {flavour} flavour")
+sizes = ["large", "medium", "small"]
+while(size := input(f"Please select the size. Available sizes are ({sizes}): ").lower()) not in sizes:
+    print(f"{size} not available. Please try again..")
+print(f"You have choosen {size}")
