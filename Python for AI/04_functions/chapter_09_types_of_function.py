a17b49adf3f769dab14b2aed4e87f0db69473d33
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-08 08:37:15 +0600

    Types of function- Pure, impure, recursive

---
+++ Python for AI/04_functions/chapter_09_types_of_function.py
@@ -0,0 +1,17 @@
+# Pure functions
+def pure_tea(cups):
+    return cups
+#impure function-> Not recommended
+total_cups = 10
+def impure_tea(cups):
+    global total_cups
+    total_cups+=cups
+    return total_cups
+
+#Recursive function
+def recursive_tea(cups):
+    if cups == 0:
+        return "All cups been processed"
+    print(f"Currently processing {cups} cups")
+    return recursive_tea(cups-1) # iterative recursion
+print(recursive_tea(3))
