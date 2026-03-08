3460b834f8747ed418603621dcbd5fc7509f8025
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-08 08:41:43 +0600

    Types of function- lambda(anonnymous function)

--- Python for AI/04_functions/chapter_09_types_of_function.py
+++ Python for AI/04_functions/chapter_09_types_of_function.py
@@ -15,3 +15,8 @@ def recursive_tea(cups):
     print(f"Currently processing {cups} cups")
     return recursive_tea(cups-1) # iterative recursion
 print(recursive_tea(3))
+
+#Lambda function(anonnymous)
+tea_list = ["light", "strong", "strong", "ginger", "milk"]
+strong_tea = list(filter(lambda tea: tea=="strong", tea_list)) # lambda here works like a function(small kinda function without any particular name, "tea works as a variable here")
+print(strong_tea)
