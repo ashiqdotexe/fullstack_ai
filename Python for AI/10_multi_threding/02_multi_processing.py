6e12545744033a2a003497c4808c7cf75267ad69
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-15 10:40:38 +0600

    Multiprocessing-1

---
+++ Python for AI/10_multi_threding/02_multi_processing.py
@@ -0,0 +1,17 @@
+from multiprocessing import Process
+import time
+def serve_tea(name):
+    print(f"Serving {name} tea")
+    time.sleep(3)
+    print(f"End of serving {name} tea")
+
+if __name__ == "__main__":
+    tea_makers = [
+        Process(target=serve_tea, args=(tea,)) 
+        for tea in ["masala", "ginger", "min"]
+    ]
+    for process in tea_makers:
+        process.start()
+    for process in tea_makers:
+        process.join()
+    print("All tea has been served")
