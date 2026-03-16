ec61429c1dd202e92c4d36518eb514e218395c7a
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-16 16:25:20 +0600

    ProcessvsThreading- 2

--- Python for AI/10_multi_threding/10_process_one.py
+++ Python for AI/10_multi_threding/10_process_one.py
@@ -6,8 +6,8 @@ from multiprocessing import Process
 class Threading:
     def incremental(self):
         total = 0
-        print("Crunching some number")
-        for i in range(10**7):
+        print("Crunching some number with threading")
+        for i in range(10**9):
             total +=i 
         print("Crunch finished")
     def threading_main(self):
@@ -16,13 +16,13 @@ class Threading:
         [t.start() for t in thredings]
         [t.join() for t in thredings]
         end = time.time()
-        return f"{end-start}"
+        return f"{end-start:.2f} seconds"
     
-class Process:
+class Process_:
     def incremental(self):
         total = 0
-        print("Crunching some number")
-        for i in range(10**7):
+        print("Crunching some number with multi-processor")
+        for i in range(10**9):
             total +=i 
         print("Crunch finished")
     def process_main(self):
@@ -31,7 +31,10 @@ class Process:
         [t.start() for t in processes]
         [t.join() for t in processes]
         end = time.time()
-        return f"{end-start}"
-
-
-
+        return f"{end-start:.2f} seconds"
+    
+if __name__ == "__main__":
+    thread = Threading()
+    process = Process_()
+    print(f"Total time taken by thread- {thread.threading_main()}")
+    print(f"Total time taken by multi-process- {process.process_main()}")
