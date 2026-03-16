eb1457d5f44b40f6c48092c571e426802818a6a5
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-16 09:28:44 +0600

    Threading-1

---
+++ Python for AI/10_multi_threding/06_thread_1.py
@@ -0,0 +1,13 @@
+import threading
+import time 
+def boil_milk():
+    print("Start boiling milk")
+    time.sleep(2)
+    print("Finished boiling milk")
+def burn_toast():
+    print("Start toasting")
+    time.sleep(3)
+    print("Finished toasting")
+
+
+    
