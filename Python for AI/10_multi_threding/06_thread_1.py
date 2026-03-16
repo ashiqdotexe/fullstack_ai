9eae5d0a75458790fd3e7505b606cd637e493052
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-16 09:46:46 +0600

    Threading - 1- 2

--- Python for AI/10_multi_threding/06_thread_1.py
+++ Python for AI/10_multi_threding/06_thread_1.py
@@ -9,5 +9,13 @@ def burn_toast():
     time.sleep(3)
     print("Finished toasting")
 
+start = time.time()
 
-    
+t1 = threading.Thread(target=boil_milk)
+t2 = threading.Thread(target=burn_toast)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+end = time.time()
+print(f"Finished! Total time taken {end-start:.2f} seconds")
