c0455009fca6f79f38fb0526d0a8ff4cf4137847
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-17 15:20:06 +0600

    Multi task using async

---
+++ Python for AI/11_asyncio/02_multi_task.py
@@ -0,0 +1,29 @@
+import asyncio
+import time
+
+async def with_async(name):
+    print(f"Preparing {name} tea")
+    await asyncio.sleep(3)
+    print(f"Finished preparing {name} tea")
+async def with_time(name):
+    print(f"Preparing {name} tea")
+    time.sleep(3)
+    print(f"Finished preparing {name} tea")
+async def main():
+    start_1 = time.time()
+    await asyncio.gather(
+        with_async("masala"),
+        with_async("ginger"),
+        with_async("cardamom"),
+    )
+    end_1 = time.time()
+    start_2 = time.time()
+    await asyncio.gather(
+        with_time("masala"),
+        with_time("ginger"),
+        with_time("cardamom"),
+    )
+    end_2 = time.time()
+    print(f"Total time taken by async methond- {end_1-start_1:.2f} seconds")
+    print(f"Total time taken by time methond- {end_2-start_2:.2f} seconds")
+asyncio.run(main())
