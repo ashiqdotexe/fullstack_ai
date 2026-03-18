fb7e51aa1a8253360332105a6b783a92078620b8
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-18 23:44:34 +0600

    asyncio with thread-2

--- Python for AI/11_asyncio/04_asyncio_thread.py
+++ Python for AI/11_asyncio/04_asyncio_thread.py
@@ -6,7 +6,9 @@ def check_store(item):
     time.sleep(3)
     print(f"Item #{item} stored in Database")
 async def main():
+    items = ["Masala", "Ginger", "Cardamom"]
     loop = asyncio.get_running_loop()
     with ThreadPoolExecutor() as pool:
-        result = await loop.run_in_executor(pool, check_store, "Masala")
+        results = [loop.run_in_executor(pool, check_store, item) for item in items]
+        await asyncio.gather(*results)
 asyncio.run(main())
