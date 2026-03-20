0904dedbd9b6ff05fe28c10b828628e60543f0d8
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-20 23:53:00 +0600

    Default pydantic conversion-2

--- Python for AI/12_pydantic/02_default_conversion.py
+++ Python for AI/12_pydantic/02_default_conversion.py
@@ -7,3 +7,5 @@ class Product(BaseModel):
     in_stock : bool = True # We can give default value as well
 
 product_one = Product(id=1, name = "Laptop", price=29.99, in_stock=False)
+product_two = Product(id=1, name = "Mouse", price=90.12)
+print(product_one)
