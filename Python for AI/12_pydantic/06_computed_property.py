825500e6eeea463f6a5ee707d90b9d7cc6149199
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-22 00:03:32 +0600

    Computed property-2

--- Python for AI/12_pydantic/06_computed_property.py
+++ Python for AI/12_pydantic/06_computed_property.py
@@ -1,1 +1,19 @@
 from pydantic import BaseModel, computed_field
+
+
+class OrderPrice(BaseModel):
+    price: float
+    quantity: int
+
+    @computed_field
+    @property  # when we use this, the function next to it becomes the property of the class itself
+    def calculated_price(self) -> float:
+        return self.price * self.quantity
+
+
+order_price = OrderPrice(price=297.7, quantity=2)
+print(f"The price is {order_price.calculated_price}")
+# To see the whole thing inside the class we can use modeldump()
+print(
+    order_price.model_dump()
+)  # You will see that the "calculated_price" itself has become a property of the class itself alongside with price and quantity
