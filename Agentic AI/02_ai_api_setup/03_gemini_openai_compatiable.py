6c5b5e02057f4f6c982da477bac7517950ac1567
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-25 13:03:07 +0600

    OpenAI with Gemini compatiblity

---
+++ Agentic AI/02_ai_api_setup/03_gemini_openai_compatiable.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os 
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+client = OpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url= "https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response = client.chat.completions.create(
+    model= "gemini-3-flash-preview",
+    messages=[
+        {
+            "role" : "system", 
+            "content" : "You know may historical things. Alongside answering the questions, please provide a one line of history insight as well"
+        }, 
+        {
+            "role" : "user",
+            "content": "What is the capital city of Bangladesh?" 
+        }
+    ]
+)
+print(response.choices[0].message.content)
