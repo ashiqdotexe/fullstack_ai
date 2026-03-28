abb76820f98331de2e99bf11ab07d9320a6f677e
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-28 11:52:28 +0600

    AI setup for weather agent

--- Agentic AI/04_ai_agents/01_weather_agent.py
+++ Agentic AI/04_ai_agents/01_weather_agent.py
@@ -1,8 +1,18 @@
-import requests
+import requests, os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+client = OpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
 
 def get_weather(city: str):
     response = requests.get(f"https://wttr.in/{city.lower()}?format=j1")
     data = response.json()
     current_weather = data.get("current_condition", {})[0].get("temp_C", {})
     return current_weather
-print(get_weather("Khulna"))
