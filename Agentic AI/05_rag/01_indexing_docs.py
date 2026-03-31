99e2ae639c84da932e30ed7e82c1a56704771b15
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-31 14:25:46 +0600

    Splitted Documents

--- Agentic AI/05_rag/01_indexing_docs.py
+++ Agentic AI/05_rag/01_indexing_docs.py
@@ -4,4 +4,9 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 file_path = Path(__file__).parent / "temp_uploaded.pdf"
 loader = PyPDFLoader(file_path)
 docs = loader.load()
-# chuncking documents
+# chunking documents
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 400
+)
+splited_docs = text_splitter.split_documents(documents=docs)
