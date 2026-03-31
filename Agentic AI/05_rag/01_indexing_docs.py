5271a5148a8538014c0638df61ef52dfbd708846
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-31 14:24:08 +0600

    Text splitter mechanism setup

--- Agentic AI/05_rag/01_indexing_docs.py
+++ Agentic AI/05_rag/01_indexing_docs.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 file_path = Path(__file__).parent / "temp_uploaded.pdf"
-pdf_reader = PyPDFLoader(file_path)
-docs = pdf_reader.load()
-print(docs[0])
+loader = PyPDFLoader(file_path)
+docs = loader.load()
+# chuncking documents
