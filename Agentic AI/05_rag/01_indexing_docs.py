378ddc16a314bd3f3f9905da4052429a6d54f4c1
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-03-31 15:53:37 +0600

    Vector Embedding and storing is done

--- Agentic AI/05_rag/01_indexing_docs.py
+++ Agentic AI/05_rag/01_indexing_docs.py
@@ -1,6 +1,16 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+
 file_path = Path(__file__).parent / "temp_uploaded.pdf"
 loader = PyPDFLoader(file_path)
 docs = loader.load()
@@ -9,4 +19,22 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_size = 1000,
     chunk_overlap = 400
 )
-splited_docs = text_splitter.split_documents(documents=docs)
+chunks = text_splitter.split_documents(documents=docs)
+
+# Embedding Documents
+embedding_model = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-2-preview",
+    api_key=GEMINI_API_KEY
+)
+# vector = embedding_model.embed_query("Hello world")
+
+# Storing in the vector Database
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding= embedding_model,
+    url = "http://localhost:6333/",
+    # url = "https://5078da44-760e-401c-bb81-bf9a27b3cce7.sa-east-1-0.aws.cloud.qdrant.io", # Use it for cloud storage cloud.qdrant.io
+    # api_key = QDRANT_API_KEY,
+    collection_name = "TEST Documents"
+)
+print("Documents embedded done...")
