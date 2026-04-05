bbdc2ebfebc4bb648084b3040cd79a20744cf54f
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-04-05 12:12:59 +0600

    Finalising multimodal ai

--- Agentic AI/05_rag/04_multi_modal.py
+++ Agentic AI/05_rag/04_multi_modal.py
@@ -1,4 +1,35 @@
-print("Testing multimodal")
-for i in range(6):
-    print(i)
-print("client request through modal")
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+client = OpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages=[
+        {
+            "role" : "system",
+            "content" : "You are multimodal AI who perfectly counts the notes"
+        },
+        {
+            "role" : "user",
+            "content" : [
+                {
+                    "type" : "text",
+                    "text" : "Please count the notes and tell me how much money is here?" 
+                },
+                {
+                    "type" : "image_url",
+                    "image_url" : {
+                        "url" : "https://upload.wikimedia.org/wikipedia/commons/a/a8/Taka_Banknotes_specimen.jpg"
+                    }
+                }
+            ]
+        }
+    ]
+)
+print(response.choices[0].message.content)
