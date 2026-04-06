fd132cb5e3502aa9d4dc837e84659298631c196e
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-04-06 15:58:56 +0600

    Basic graph builder for langgraph

---
+++ Agentic AI/06_langgraph/01_graph-setup.py
@@ -0,0 +1,8 @@
+from typing_extensions import Annotated, TypedDict
+from langgraph.graph import StateGraph
+from langgraph.graph.message import add_messages
+
+class State(TypedDict):
+    message : Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
