7390dc082c4ff318e2f9f8668a48a3454622e35f
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-04-07 13:27:50 +0600

    Building agent inside graph-1

--- Agentic AI/06_langgraph/01_graph-setup.py
+++ Agentic AI/06_langgraph/01_graph-setup.py
@@ -2,7 +2,9 @@ from typing_extensions import Annotated, TypedDict
 from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
 
+
 class State(TypedDict):
     message : Annotated[list, add_messages]
 
 graph_builder = StateGraph(State)
+print("Building agent through graph")
