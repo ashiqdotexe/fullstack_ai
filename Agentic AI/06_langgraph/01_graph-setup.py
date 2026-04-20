70556ff39a59e53e8adcac5162804ab13e794bd2
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-04-20 11:38:00 +0600

    Adding nodes to the state

--- Agentic AI/06_langgraph/01_graph-setup.py
+++ Agentic AI/06_langgraph/01_graph-setup.py
@@ -6,3 +6,21 @@ from langgraph.graph import StateGraph, START, END
 class State(TypedDict):
     messages : Annotated[list, add_messages]
 build_graph = StateGraph(State)
+
+#Creating nodes
+
+def chatbot(state: State):
+    return {"messages" : ["Hi This is chatbot tool"]}
+
+def samplenode(state: State):
+    return {"messages" : ["Hi this is sample node"]}
+
+
+"""
+The messages will be keep appending to the state
+
+"""
+# Registering nodes to state
+
+build_graph.add_node("Chatbot", chatbot)
+build_graph.add_node("Sample Node", samplenode)
