443a127e4922767911f679e87e4b416d468f10d8
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2026-04-20 11:02:17 +0600

    State builder -1 for langgraph

--- Agentic AI/06_langgraph/01_graph-setup.py
+++ Agentic AI/06_langgraph/01_graph-setup.py
@@ -1,10 +1,8 @@
-from typing_extensions import Annotated, TypedDict
-from langgraph.graph import StateGraph
+from typing_extensions import TypedDict
+from typing import Annotated
 from langgraph.graph.message import add_messages
-
+from langgraph.graph import StateGraph, START, END
 
 class State(TypedDict):
-    message : Annotated[list, add_messages]
-
-graph_builder = StateGraph(State)
-print("Building agent through graph")
+    messages : Annotated[list, add_messages]
+build_graph = StateGraph(State)
